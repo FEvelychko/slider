b7a086f1247b4abe37889667341452918c7ecea9
FEvelychko <maxdevelopenko@gmail.com>
2015-09-18 20:29:12 +0300

    border behaviour and duration

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
-/**
- * Created by Maksym on 17.09.2015.
- */
+    /**
+     * Created by Maksym on 11.09.2015.
+     */
+
 
     //debugger;
 
@@ -10,8 +11,10 @@
     var imageWidth;
     var currentImage;
     var prev, next;
+    var x,y;
 
     function slider(){
+
         currentImage = 0;
         ul = document.getElementById('image_slider');
         liItems = ul.children;
@@ -20,18 +23,49 @@
         ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
         prev = document.getElementById("prev");
         next = document.getElementById("next");
-        next.addEventListener('click', getNext, false);
-        prev.addEventListener('click', getPrevious, false);
+        next.addEventListener('click', setNextDuration, false);
+        prev.addEventListener('click', setPrevDuration, false);
+
+    }
+
+    function setNextDuration(){
+        x = setInterval(getNext, 300);
+    }
+
+    function setPrevDuration(){
+        y = setInterval(getPrevious, 300);
+    }
+
+    function showCurrentImage(){
+        console.log(currentImage);
+    }
+
+    function borderBehave(){
+        if(currentImage === 5){
+            currentImage = 0;
+            ul.style.left = -parseInt(imageWidth * currentImage) + 'px';
+        }
+        else if(currentImage === -1){
+            currentImage = 4;
+            ul.style.left = -parseInt(imageWidth * currentImage) + 'px';
+        }
     }
 
     function getNext(){
         currentImage += 1;
         ul.style.left = -parseInt(imageWidth * currentImage) + 'px';
+        showCurrentImage();
+        clearInterval(x);
+        borderBehave();
     }
+
 
     function getPrevious(){
         currentImage -= 1;
         ul.style.left = -parseInt(imageWidth * currentImage) + 'px';
+        showCurrentImage();
+        clearInterval(y);
+        borderBehave();
     }
 
     window.onload = slider();
